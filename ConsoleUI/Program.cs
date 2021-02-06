fb8d04519cea155717a5aee438a0a6aac916b75e
melenglobal <75787400+melenglobal@users.noreply.github.com>
2021-02-06 17:26:26 +0300

    Added EntityFramework

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -1,17 +1,18 @@
 ﻿using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
 using DataAccess.Concrete.InMemory;
 using System;
 
 namespace ConsoleUI
 {
     class Program
-    {
+    {    //SOLİD O- Open Closed Principle
         static void Main(string[] args)
         {
 
-            ProductManager productManager = new ProductManager(new InMemoryProductDal()); // Beni yenileyebilmen için, hangi veri yöntemi ile çalıştığını bana söyle zahmet olmaz ise.
+            ProductManager productManager = new ProductManager(new EfProductDal()); // Beni yenileyebilmen için, hangi veri yöntemi ile çalıştığını bana söyle zahmet olmaz ise.
 
-            foreach (var product in productManager.GetAll())
+            foreach (var product in productManager.GetByUnitPrice(75,100))
             {
                 Console.WriteLine(product.ProductName);
             }
