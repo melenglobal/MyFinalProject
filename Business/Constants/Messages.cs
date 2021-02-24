59cb44a4143edb4e5ce91b02637dfd43c3f0996e
melenglobal <75787400+melenglobal@users.noreply.github.com>
2021-02-24 22:01:31 +0300

    Code Refactoring

--- Business/Constants/Messages.cs
+++ Business/Constants/Messages.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Entities.Concrete;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -8,6 +9,7 @@ namespace Business.Constants
     {
         public static string ProductAdded = "Ürün Eklendi.";
         public static string ProductNameInvalid = "Ürün İsmi Geçersiz!";
-        
+        internal static List<Product> MaintenanceTime;
+        internal static string ProductsListed;
     }
 }
