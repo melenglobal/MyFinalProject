de7a060f6c73e2a879c9c7cce5ff6738daf002dd
melenglobal <75787400+melenglobal@users.noreply.github.com>
2021-02-26 08:48:36 +0300

    Final

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -29,10 +29,10 @@ namespace Business.Concrete
 
         public IDataResult<List<Product>> GetAll()
         {
-            if (DateTime.Now.Hour == 22)
-            {
+           if (DateTime.Now.Hour == 8)
+           {
                 return new ErrorDataResult<List<Product>>(Messages.MaintenanceTime);
-            }
+           }
             // iş kodları ?
             // Yetkili mi ?
             return new SuccessDataResult<List<Product>>(_productDal.GetAll(),Messages.ProductsListed);
@@ -56,10 +56,10 @@ namespace Business.Concrete
 
         public IDataResult<List<ProductDetailDto>> GetProductDetails()
         {
-            if (DateTime.Now.Hour == 22)
-            {
-                return new ErrorDataResult<List<ProductDetailDto>>(Messages.MaintenanceTime);
-            }
+            //if (DateTime.Now.Hour == 22)
+            //{
+            //    return new ErrorDataResult<List<ProductDetailDto>>(Messages.MaintenanceTime);
+            //}
             return new SuccessDataResult<List<ProductDetailDto>>
                 (_productDal.GetProductDetails());
         }
