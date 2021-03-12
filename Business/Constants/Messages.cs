ffc9b85566258453423b42af907ba356e24b7d49
melenglobal <75787400+melenglobal@users.noreply.github.com>
2021-03-12 09:38:43 +0300

    Core Module Added

--- Business/Constants/Messages.cs
+++ Business/Constants/Messages.cs
@@ -1,6 +1,8 @@
-﻿using Entities.Concrete;
+﻿using Core.Entities.Concrete;
+using Entities.Concrete;
 using System;
 using System.Collections.Generic;
+using System.Runtime.Serialization;
 using System.Text;
 
 namespace Business.Constants
@@ -14,5 +16,14 @@ namespace Business.Constants
         public static string ProductCountOfCategoryError = "Sepetiniz doldu taştı !";
         public static string ProductNameAlreadyExists = "Bir ipte iki cambaz oynamaz!(Bu isimde başka bir ürün zaten var)";
         public static string CategoryLimitExceded = "Basit bir şekilde Anasayfaya mı tıklasan ?";
+        public static string AuthorizationDenied = "Hop Birader nereye ?";
+
+        public static string UserRegistered = "Çırak koş iki çay kap bize,buyrun buyrun hoşgeldiniz!";
+        public static string SuccessfulLogin = "Özlettiniz cüzdanınızı,nerelerdesiniz?";
+        public static string UserAlreadyExists = "Git bak bakalım ben içerdemiyim?-(Başka bir kullanıcı adı deneyiniz.)";
+        public static string AccessTokenCreated ="Başarılı bir şekilde süper kahraman girişi yaptınız!";
+
+        public static string UserNotFound = "Aradım,taradım valla böyle birisini bulamadım.";
+        public static string PasswordError = "Girdiğin şifre ya da kullanıcı adın ile elimdeki bilgiler eşleşmiyor.Bi kaç tur dene istersen.";
     }
 }
