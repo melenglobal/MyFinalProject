343bcf4cadeb5a857a40f1a1ca5f4abecc782baf
melenglobal <75787400+melenglobal@users.noreply.github.com>
2021-03-22 22:18:29 +0300

    extension injection error solved.

--- Core/Aspects/Autofac/Caching/CacheRemoveAspect.cs
+++ Core/Aspects/Autofac/Caching/CacheRemoveAspect.cs
@@ -5,6 +5,7 @@ using Core.Utilities.IoC;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace Core.Aspects.Autofac.Caching
 {
